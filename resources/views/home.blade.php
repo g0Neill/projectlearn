6dffd693d6f9c72580317aa0486ce899065b0854
Gulliver O'Neill <gulliver@foxandbeard.co.za>
2017-03-28 14:38:41 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,68 @@
+ {{--* Created by PhpStorm.--}}
+ {{--* User: Gulliver--}}
+ {{--* Date: 3/27/2017--}}
+ {{--* Time: 10:24 PM--}}
+
+
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Project Learn</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- ICONS  -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+        <!-- CSS -->
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/skeleton.css">
+        <link rel="stylesheet" href="css/custom.css">
+        <link rel="stylesheet" href="css/animation.css">
+
+        <!-- Favicon -->
+        <link rel="icon" type="image/png" href="images/favicon.png">
+
+    </head>
+    <body>
+        <!-- Primary Page Layout -->
+        <div class="container">
+            <div class="row">
+                <div class="twelve columns logo">
+                    <img src="images/gorilla.jpg" style="max-width: 100%;" alt="Gorilla">
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="six columns">
+                    <div class="contact">
+                        <h4>Who I Am</h4>
+                        <p>Self study is my way of learning at this time. I am motivated to reach my goal of becoming a software developer. I am a Web Developer specializing in front-end development. Experience is limited due to a recent career change but an eagerness to learn will overcome any lack of experience quickly. Experience carrying over from other fields includes managing people, project management and problem solving under pressure. I enjoy a challenge and learning new tools and technologies to use. At the moment I am interested in learning PHP and increasing my knowledge and skills in JavaScript.</p>
+                    </div>
+                    <div class="details">
+                        <h4>I dunno</h4>
+                    </div>
+                </div>
+                <div class="six columns">
+                    <div class="right-stuff">
+                        <h4>SOME MORE STUFF</h4>
+                    </div>
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="twelve columns">
+                    <h4>Some Info</h4>
+                </div>
+            </div>
+        </div>
+    </body>
+</div>
+</div>
+</body>
+</html>
